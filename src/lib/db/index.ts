8e6f741d8b9a8e62cb90348d8cebefe5202294d6
Rithi-20 <rithihaumani2004@gmail.com>
2025-11-21 22:01:38 +0530

    Fix: enable SSL for Neon connection

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -11,10 +11,13 @@
 // const client = postgres(connectionString, { max: 1 });
 
 // export const db = drizzle(client, { schema });
-
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(process.env.POSTGRES_URL!, { ssl: false });
+const client = postgres(process.env.POSTGRES_URL!, {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const db = drizzle(client);
